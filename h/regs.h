90a5a85c9640eb516df471c1a9a3b8eb7752bbdd
legendlc <llc_legend@163.com>
2016-06-08 22:41:05 +0800

    Change the definition of physical address from 0x20****** to 0x3F******

--- h/regs.h
+++ h/regs.h
@@ -2,27 +2,27 @@
 #ifndef REGS_H_
 #define REGS_H_
 
-#define GPFSEL1 0x20200004
-#define GPSET0  0x2020001C
-#define GPCLR0  0x20200028
-#define GPPUD       0x20200094
-#define GPPUDCLK0   0x20200098
+#define GPFSEL1 0x3F200004
+#define GPSET0  0x3F20001C
+#define GPCLR0  0x3F200028
+#define GPPUD       0x3F200094
+#define GPPUDCLK0   0x3F200098
 
-#define AUX_ENABLES     0x20215004
-#define AUX_MU_IO_REG   0x20215040
-#define AUX_MU_IER_REG  0x20215044
-#define AUX_MU_IIR_REG  0x20215048
-#define AUX_MU_LCR_REG  0x2021504C
-#define AUX_MU_MCR_REG  0x20215050
-#define AUX_MU_LSR_REG  0x20215054
-#define AUX_MU_MSR_REG  0x20215058
-#define AUX_MU_SCRATCH  0x2021505C
-#define AUX_MU_CNTL_REG 0x20215060
-#define AUX_MU_STAT_REG 0x20215064
-#define AUX_MU_BAUD_REG 0x20215068
+#define AUX_ENABLES     0x3F215004
+#define AUX_MU_IO_REG   0x3F215040
+#define AUX_MU_IER_REG  0x3F215044
+#define AUX_MU_IIR_REG  0x3F215048
+#define AUX_MU_LCR_REG  0x3F21504C
+#define AUX_MU_MCR_REG  0x3F215050
+#define AUX_MU_LSR_REG  0x3F215054
+#define AUX_MU_MSR_REG  0x3F215058
+#define AUX_MU_SCRATCH  0x3F21505C
+#define AUX_MU_CNTL_REG 0x3F215060
+#define AUX_MU_STAT_REG 0x3F215064
+#define AUX_MU_BAUD_REG 0x3F215068
 
 //interrupt control regs
-#define BASE_INTC			(0x2000B200)
+#define BASE_INTC			(0x3F00B200)
 
 typedef struct {
 	unsigned long	IRQBasic;	// Pending 0
@@ -40,7 +40,7 @@ typedef struct {
 static volatile INTC_REGS * const intcRegs = (INTC_REGS *) (BASE_INTC);
 
 //timer regs
-#define portTIMER_BASE                    		( (unsigned long ) 0x2000B400 )
+#define portTIMER_BASE                    		( (unsigned long ) 0x3F00B400 )
 
 typedef struct _BCM2835_TIMER_REGS {
 	unsigned long LOD;
